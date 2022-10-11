d1f52cfc7146be3537a2eab009f9a2b8ba3558ed
Yuriko <ygabriela.arnott@gmail.com>
2022-10-10 22:56:15 -0400

    commit tree 5675ae2d71355d11e73a2dd4def1fafb5cbbb243

--- index.js
+++ index.js
@@ -1,11 +1,35 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
+function scuberGreetingForFeet(someNumber){ 
+let result
+  if (someNumber <= 400){
+      result = 'This one is on me!';
+    }  else if(someNumber > 400 && someNumber < 2000) {
+      result = 'That will be twenty bucks.';
+    } else if(someNumber > 2000 && someNumber < 2500){
+      result = 'I will gladly take your thirty bucks.';
+    } else {
+      result = 'No can do.';
+    }
+  return result
+  }
+
+function ternaryCheckCity(X){
+  let result1
+   X =='NYC' ? (result1 = 'Ok, sounds good.') : (result1 = 'No go.');
+  return result1;
 }
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
-
-function switchOnCharmFromTip(){
-  // Write your code here!
+function switchOnCharmFromTip(tip){
+  let result2
+  switch(tip){
+    case 'generous':
+      result2 = "Thank you so much.";
+      break;
+    case 'not as generous':
+      result2 = "Thank you.";
+      break;
+    default :
+      result2 = "Bye.";
+      break;
+  }
+  return result2
 }
